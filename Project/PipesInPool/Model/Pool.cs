af25f19b3b712860114880943595280aa8f80920
Whufy <127833502+Bojkata123@users.noreply.github.com>
2023-12-02 15:37:32 +0200

    Add files via upload
    Zadacha

---
+++ Project/PipesInPool/Model/Pool.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipesInPool.Model
+{
+     class Pool
+    {
+        private double volume;
+        private double pipe1Flow;
+        private double pipe2Flow;
+        private double hours;
+        public double Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+        public double PipeFlow2
+        {
+            get { return pipe2Flow; }
+            set { pipe2Flow = value; }
+        }
+        public double PipeFlow1
+        {
+            get { return pipe1Flow; }
+            set { pipe1Flow = value; }
+        }
+        public double Volume
+        {
+            get { return volume; }
+            set { volume = value; }
+        }
+        public Pool(double volume, double pipeFlow1, double pipeFlow2, double hours)
+        {
+            this.Volume = volume;
+            this.PipeFlow1 = pipeFlow1;
+            this.PipeFlow2 = pipeFlow2;
+            this.Hours = hours;
+        }
+
+        public Pool() : this(0, 0, 0, 0) { } // Prazen konstruktor koito durji stoinosti dokato se napulni gavniq konstruktor
+
+        public double Pipe1MultiplyByhours() //truba 1 napulnila N litri
+        {
+            return pipe1Flow * hours;
+        }
+        public double Pipe2MultiplyByhours() //truba 2 napulnila N litri
+        {
+            return pipe2Flow * hours;
+        }
+        public double SumPipesFlow() // truba 1 i truba 2 obshto litri
+        {
+            return Pipe1MultiplyByhours() + Pipe2MultiplyByhours();
+        }
+        public double PercentageFlowFromPipe1AndPipe2FromVolume() // kolko procenta sa napulnili truba1 i truba2 ot celiq obem
+        {
+            return (SumPipesFlow() / volume) * 100;
+        }
+
+        public double Pipe1FlowPercentage() // kolko % e napulnila truba 1
+        {
+            return (Pipe1MultiplyByhours() / SumPipesFlow() * 100);
+        }
+
+        public double Pipe2FlowPercentage() // kolko % e napulnila truba 2
+        {
+            return (Pipe2MultiplyByhours() / SumPipesFlow() * 100);
+        }
+
+    }
+}
---
+++ Project/PipesInPool/Program.cs
@@ -0,0 +1,19 @@
+﻿using PipesInPool.Controlles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipesInPool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PoolController myController = new PoolController();
+
+
+        }
+    }
+}
